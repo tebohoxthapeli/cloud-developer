7389ae61bcbeb6457fd2e265855e4fc7ee467321
Teboho Thapeli <tm.thapeli@gmail.com>
2022-06-13 15:12:35 +0200

    modified frontend

--- course-02/exercises/udacity-c2-frontend/src/app/api/api.module.ts
+++ course-02/exercises/udacity-c2-frontend/src/app/api/api.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { ApiService } from './api.service';
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+// import { ApiService } from "./api.service";
 
 const components = [];
 
 @NgModule({
-  imports: [
-    HttpClientModule,
-  ],
-  declarations: components,
-  exports: components,
-  providers: []
+    imports: [HttpClientModule],
+    declarations: components,
+    exports: components,
+    providers: [],
 })
+
 export class ApiModule {}
